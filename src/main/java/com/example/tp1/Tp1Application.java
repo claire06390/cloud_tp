036a2d18136860001b1317ed38a1db0038ab0153
Marini Claire <66369809+claire06390@users.noreply.github.com>
2023-09-12 18:13:34 +0200

    supp db

--- src/main/java/com/example/tp1/Tp1Application.java
+++ src/main/java/com/example/tp1/Tp1Application.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-@EntityScan(basePackages = {"com.example.tp1"}) // add this so the spring boot context knows where to look for entities
 public class Tp1Application {
 
     public static void main(String[] args) {
